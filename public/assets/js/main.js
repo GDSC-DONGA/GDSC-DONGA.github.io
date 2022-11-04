89cd37ca921a288bab3902f43639333543d7019a
JOyagdol <joyagdol1224@gmail.com>
2022-11-04 15:24:09 +0900

    feat: time변경

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -84,7 +84,7 @@ var spy = new Gumshoe('#navigation a', {
 
 /* ======= Countdown ========= */
 // set the date we're counting down to
-var target_date = new Date("Nov 5, 2022").getTime();
+var target_date = new Date("2022-11-05 13:30:00").getTime();
  
 // variables for time units
 var days, hours, minutes, seconds;
